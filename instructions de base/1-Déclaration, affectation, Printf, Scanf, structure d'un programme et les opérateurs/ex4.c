bf2482308fc15ba67283206782a63b00fd672ffa
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2024-01-15 21:55:32 +0100

    Update ex4.c

--- instructions de base/1-Déclaration, affectation, Printf, Scanf, structure d'un programme et les opérateurs/ex4.c
+++ instructions de base/1-Déclaration, affectation, Printf, Scanf, structure d'un programme et les opérateurs/ex4.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+
+int main(){
+	
+	float a,b,s,p,df,dv;
+	
+	
+	
+	
+	printf("Veullez enter la Valeur de A :  ");
+	scanf("%f",&a);
+	
+	printf("Veullez enter La valeur de B :  ");
+	scanf("%f",&b);
+	s = a + b;
+	p = a * b;
+	df = a - b;
+	dv = a / b;
+	
+	printf("A + B = %.2f \n",s);
+	printf("A * B = %.2f \n",p);
+	printf("A - B = %.2f \n",df);
+	printf("A / B = %.2f \n",dv);
+	
+
+}
 
