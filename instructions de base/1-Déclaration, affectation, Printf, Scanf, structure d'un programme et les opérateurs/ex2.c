12d1fd6a0c577a162a996ad67364c2c52c0d5af4
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2024-01-15 21:54:07 +0100

    Update ex2.c

--- instructions de base/1-Déclaration, affectation, Printf, Scanf, structure d'un programme et les opérateurs/ex2.c
+++ instructions de base/1-Déclaration, affectation, Printf, Scanf, structure d'un programme et les opérateurs/ex2.c
@@ -1,1 +1,24 @@
 
+#include <stdio.h>
+
+
+int main(){
+	int naisse,age;
+	
+	float largeur,longeur,p,s;
+	
+	
+	printf("Veullez enter la largeur du rectangle :  ");
+	scanf("%f",&largeur);
+	
+	printf("Veullez enter la longeur du rectangle :  ");
+	scanf("%f",&longeur);
+	
+	p = (largeur + longeur) * 2;
+	s = largeur * longeur ;
+	
+	printf("Le perimetre du rectangle est %.2f . \n",p);
+	printf("La surface du rectangle est %.2f . \n",s);
+	
+
+}
