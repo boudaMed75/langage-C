e81b5ba02574a9389a16c1b2dab7c6e5584c3998
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2024-01-15 21:54:49 +0100

    Update ex3.c

--- instructions de base/1-Déclaration, affectation, Printf, Scanf, structure d'un programme et les opérateurs/ex3.c
+++ instructions de base/1-Déclaration, affectation, Printf, Scanf, structure d'un programme et les opérateurs/ex3.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+	
+	float x,y,p;
+	
+	
+	printf("Donner la valeur de X :  ");
+	scanf("%f",&x);
+	
+	printf("Donner la valeur de Y :  ");
+	scanf("%f",&y);
+	
+	p = pow(x,y);
+	
+	
+	
+	printf("La puissance est : %.2f . \n",p);
+
+	
+
+}
