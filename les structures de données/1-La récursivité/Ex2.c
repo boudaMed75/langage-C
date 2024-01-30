140941b5d00db98d2b8a41afe27405c4b8988031
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2024-01-30 18:23:05 +0100

    Update Ex2.c

--- les structures de données/1-La récursivité/Ex2.c
+++ les structures de données/1-La récursivité/Ex2.c
@@ -1,1 +1,18 @@
 
+#include<stdio.h>
+
+int fibancci(int n){
+	
+	if(n <= 1) return 1;
+	return fibancci(n-1) + fibancci(n-2);
+}
+
+int main(){
+	int n;
+	printf("Entrer un entier n : ");
+	printf("%d",&n);
+	
+	printf("Le fibonacci de %d est : %d ",n,fibancci(n));
+	
+	
+}
