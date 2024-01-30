881932a8481464815277150d234d73bd81b05a18
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2024-01-30 18:26:17 +0100

    Update Ex5.c

--- les structures de données/1-La récursivité/Ex5.c
+++ les structures de données/1-La récursivité/Ex5.c
@@ -1,1 +1,17 @@
 
+#include<stdio.h>
+
+
+void affiche_recu(int t[],int n , int i){
+	if( i==n){
+		printf("%3d",t[i]);
+		affiche_recu(t,n,i+1);
+	}
+}
+
+void affiche_recu2(int t[],int n){
+	if(n<0) return ;
+	affiche_recu(t,n-1);
+	printf("%3d",t[n]);
+}
+
