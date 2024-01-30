447f1d5f0b2e801bc29d07a015263b84788b66aa
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2024-01-30 18:22:01 +0100

    Update ex1.c

--- les structures de données/1-La récursivité/ex1.c
+++ les structures de données/1-La récursivité/ex1.c
@@ -1,8 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    // Displaying "Hello, World!" on the screen
-    printf("Hello, World!\n");
+int fact_iter(int n){
+	
+	int fact = 1;
+	for(int i = n; i>= 1;i++){
+		fact = fact * i;
+	}
+	return fact;
+}
 
-    return 0;
+int fact_recu(int n){
+	
+	if(n==0) return n;
+	
+	else return n * fact_recu(n-1);
+}
+
+int main(){
+	int n;
+	printf("Entrer un entier n : ");
+	printf("%d",&n);
+	
+	printf("Le factoriel de %d est : %d ",n,fact_iter(n));
+	printf("Le factoriel de %d est : %d ",n,fact_recu(n));
+	
 }
