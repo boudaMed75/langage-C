332346fcb07b94f27f5e69829f3b62eeaf93dfc9
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2024-02-03 22:17:45 +0100

    Update ex1.c

--- les structures de données/2-Les méthodes de tri et de recherche dans un tableau/ex1.c
+++ les structures de données/2-Les méthodes de tri et de recherche dans un tableau/ex1.c
@@ -1,8 +1,55 @@
 #include <stdio.h>
 
+#define MAX 100
+
+int posMin(int tab[], int courant, int taille) {
+    int pos_min = courant;
+    
+    for (int i = courant + 1; i < taille; i++) {
+        if (tab[i] < tab[pos_min]) {
+            pos_min = i;
+        }
+    }
+    
+    return pos_min;
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void triParSelection(int tab[], int taille) {
+    for (int i = 0; i < taille - 1; i++) {
+        int min = posMin(tab, i, taille);
+        swap(&tab[i], &tab[min]);
+    }
+}
+
 int main() {
-    // Displaying "Hello, World!" on the screen
-    printf("Hello, World!\n");
+    int tableau[MAX];
+    int taille;
+
+    // Demander la taille du tableau à l'utilisateur
+    printf("Entrez la taille du tableau (max %d) : ", MAX);
+    scanf("%d", &taille);
+
+    // Lire les valeurs du tableau
+    printf("Entrez les valeurs du tableau :\n");
+    for (int i = 0; i < taille; i++) {
+        printf("Element %d : ", i + 1);
+        scanf("%d", &tableau[i]);
+    }
+
+    // Trier le tableau par sélection
+    triParSelection(tableau, taille);
+
+    // Afficher le tableau trié
+    printf("\nTableau trié :\n");
+    for (int i = 0; i < taille; i++) {
+        printf("%d ", tableau[i]);
+    }
 
     return 0;
 }
