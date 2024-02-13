57342d44eb695f26625c50d34f0c74df4c8705c7
Mohamed Bouda <126961762+boudaMed75@users.noreply.github.com>
2024-02-13 16:25:21 +0100

    Update ex1.c

--- les structures de données/3- Les pointeurs/ex1.c
+++ les structures de données/3- Les pointeurs/ex1.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 
-int main() {
-    // Displaying "Hello, World!" on the screen
-    printf("Hello, World!\n");
+#define SIZE 5 // Taille du tableau
 
+// Fonction pour inverser les éléments du tableau
+void inverserTableau(int *debut, int *fin) {
+    while (debut < fin) {
+        int temp = *debut;
+        *debut = *fin;
+        *fin = temp;
+        debut++;
+        fin--;
+    }
+}
+
+int main() {
+    int tableau[SIZE];
+    
+    // Remplissage du tableau
+    printf("Entrez %d entiers :\n", SIZE);
+    for (int i = 0; i < SIZE; i++) {
+        printf("Élément %d : ", i+1);
+        scanf("%d", &tableau[i]);
+    }
+    
+    // Affichage du tableau initial
+    printf("\nTableau initial :\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", tableau[i]);
+    }
+    printf("\n");
+    
+    // Inversion des éléments du tableau en utilisant deux pointeurs
+    int *debut = tableau;
+    int *fin = tableau + SIZE - 1;
+    inverserTableau(debut, fin);
+    
+    // Affichage du tableau inversé
+    printf("\nTableau inversé :\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", tableau[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
